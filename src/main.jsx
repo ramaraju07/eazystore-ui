873ad41621be0555b173a19856e94f320e35a72f
ramaraju07 <rama5raj5@gmail.com>
2026-06-30 17:28:23 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+if (1 === 1) {
+  console.log('Running in development mode');
+} else { console.log('Running in production mode'); }
+
+bmd
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
